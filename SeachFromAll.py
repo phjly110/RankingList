7e4fc7f4f64e5dff8a136ac8af2d236df88901fc
phjly110 <379812164@qq.com>
2016-03-28 21:20:18 +0800

    输入书籍编号和日期，查看前几天的情况并绘图

--- SeachFromAll.py
+++ SeachFromAll.py
@@ -1,5 +1,8 @@
 #  _*_ coding:utf-8 _*_
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 class BookCondition:
     bookId = ''
     eventId = 0
@@ -18,91 +21,132 @@ def searchInClickCatalog(search_date, search_bookId):
 
     line = f.readline()
 
-    user_set = set()
+    dayMap = {}
+
 
     while line:
         userId,bookId,eventId,time = line.split(',')
         date = time.split(' ')[0]
-        #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        day = int(date.split('/')[2])
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
+    return dayMap
 
 def searchInDwelltime(search_date, search_bookId):
     f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/dwelltime')
 
     line = f.readline()
-
-    user_set = set()
+    dayMap = {}
 
     while line:
         userId,bookId,time = line.split(',')
         eventId = 5
         date = time.split('\t')[0]
-        #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        day = int(date.split('/')[2])
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
+    return dayMap
 
 def searchInClickCover(search_date, search_bookId):
     f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/ClickCover')
 
     line = f.readline()
 
-    user_set = set()
+    dayMap = {}
+
 
     while line:
         userId,bookId,eventId,time = line.split(',')
         date = time.split(' ')[0]
-        #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        day = int(date.split('/')[2])
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
+
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
+    return dayMap
 
 def searchInDownload(search_date, search_bookId):
     f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/Download')
 
     line = f.readline()
 
-    user_set = set()
+    dayMap = {}
+
 
     while line:
         userId,bookId,eventId,time = line.split(',')
         date = time.split(' ')[0]
-        #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        day = int(date.split('/')[2])
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
+
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
+    return dayMap
 
 def searchInDelsub(search_date, search_bookId):
     f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/Delsub')
 
     line = f.readline()
 
-    user_set = set()
+    dayMap = {}
+
 
     while line:
         userId,bookId,eventId,time = line.split(',')
         date = time.split(' ')[0]
-        #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        day = int(date.split('/')[2])
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
+
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
+    return dayMap
 
 
 def searchInAddsub(search_date, search_bookId):
@@ -110,30 +154,67 @@ def searchInAddsub(search_date, search_bookId):
 
     line = f.readline()
 
-    user_set = set()
+    dayMap = {}
+
 
     while line:
         userId,bookId,eventId,time = line.split(',')
         date = time.split(' ')[0]
+        day = int(date.split('/')[2])
         #print date
-        if bookId == search_bookId and date == search_date:
-            user_set.add(userId)
+        if bookId == search_bookId and int(search_date.split('/')[2])-8 < day < int(search_date.split('/')[2])+1:
+            if dayMap.get(date):
+                user_set = dayMap.get(date)
+                user_set.add(userId)
+                dayMap[date] = user_set
+            else:
+                user_set = set()
+                user_set.add(userId)
+                dayMap[date] = user_set
         line = f.readline()
-
+    f.close()
     print eventId
-    print len(user_set)
-    print user_set
-
+    # print len(user_set)
+    # print user_set
+    return dayMap
 
 def run():
-    date = '2013/07/24'
-    bookId = 'b0003278'
-    searchInAddsub(date,bookId)
-    searchInDelsub(date,bookId)
-    searchInDownload(date,bookId)
-    searchInClickCover(date,bookId)
-    searchInDwelltime(date,bookId)
-    searchInClickCatalog(date,bookId)
+    date = '2013/07/25'  #输入要搜索的日期
+    bookId = 'b0003278'   #输入要搜索的书籍
+    map_arr = []
+    dayMap1 = searchInAddsub(date,bookId)
+    dayMap2 = searchInDelsub(date,bookId)
+    dayMap3 = searchInDownload(date,bookId)
+    dayMap4 = searchInClickCover(date,bookId)
+    dayMap5 = searchInDwelltime(date,bookId)
+    dayMap6 = searchInClickCatalog(date,bookId)
 
+    map_arr.append(dayMap1)
+    map_arr.append(dayMap2)
+    map_arr.append(dayMap3)
+    map_arr.append(dayMap4)
+    map_arr.append(dayMap5)
+    map_arr.append(dayMap6)
+
+    #dayMap = sorted(dayMap.iteritems(), key = lambda asd:asd[0],reverse = False)
+    color_arr = ['b','c','g','k','m','r','y','b','c','g']
+    n = 8
+    X = np.arange(n) + 1
+    count_Num = 0
+    for dayMap in map_arr:
+        addSub_arr = []
+        for key in dayMap:
+            addSub_arr.append([int(date.split('/')[2])-int(key.split('/')[2]),len(dayMap.get(key))])
+        addSub_arr = sorted(addSub_arr,reverse=True)
+
+        Y = [addSub_arr[i][1] for i in range(0,len(addSub_arr))]
+        plt.bar(X+0.15*count_Num,Y,width=0.15,facecolor=color_arr[count_Num],edgecolor='white')
+        count_Num = count_Num + 1
+        print addSub_arr
+
+    plt.show()
 if __name__ == '__main__':
     run()
+    #a = [[1,2],[3,4],[5,6],[7,8],[9,0]]
+    #b = [a[i][1] for i in range(0,len(a))]
+    #print b
