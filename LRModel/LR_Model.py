46fc945b74ec4e2036a16de7cab134bd83490178
phjly110 <379812164@qq.com>
2016-05-20 22:56:48 +0800

    LR

--- LRModel/LR_Model.py
+++ LRModel/LR_Model.py
@@ -2,19 +2,25 @@
 from os import listdir
 
 def loadDate(directions):
-    
+    trainFile_lists = []
     for direction in directions:
         trainFile_list = listdir('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/featureEngineering/'+direction+'/')
         m = len(trainFile_list)
-        for trainFile in trainFile_list[1:]:
-            f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/featureEngineering/'+direction+'/'+trainFile)
-            line = f.readline()
-            while line:
-
-                line = f.readline()
-            f.close()
-        print trainFile_list[1:]
+        trainFile_lists.append(trainFile_list[1:])
+        #print trainFile_list[1:]
         print m
+    print trainFile_lists
+    print len(trainFile_lists)
+    for i in range(len(trainFile_lists):
+        
+        # for trainFile in trainFile_list[1:]:
+        #     f = open('/Users/phj/Documents/Postgraduate/BookData/BooksPredict/OriginalData/featureEngineering/'+direction+'/'+trainFile)
+        #     line = f.readline()
+        #     while line:
+        #
+        #         line = f.readline()
+        #     f.close()
+
 
     return 1
 
